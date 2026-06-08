c520e8131cbc272aa812d45e61986c7b9e2aa78a
Om Maurya <ommaurya2580@gmail.com>
2026-06-08 11:50:24 +0530

    Add palindrome checking function
    Implement isPalindrome function to check if a string is a palindrome, ignoring non-alphanumeric characters and case.

---
+++ Week1/Day5/ValidPalindrome.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+
+        string str = "";
+
+        for(char ch : s) {
+            if(isalnum(ch)) {
+                str += tolower(ch);
+            }
+        }
+
+        int left = 0;
+        int right = str.size() - 1;
+
+        while(left < right) {
+            if(str[left] != str[right])
+                return false;
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};
