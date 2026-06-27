24a763523bb29acc8754b52208f1f6dd3834de6b
Om Maurya <ommaurya2580@gmail.com>
2026-06-27 12:21:54 +0530

    Create ToTestInt.java

---
+++ Week4/Day6/OOPS/ToTestInt.java
@@ -0,0 +1,9 @@
+public class ToTestInt {
+
+    public static void main(String[] args) {
+
+        Arithmetic obj = new Arithmetic();
+
+        System.out.println("Square of 5 = " + obj.square(5));
+    }
+}
