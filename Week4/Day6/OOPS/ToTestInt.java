071ca078ddf8c3855e6c78228e1d6a594ebdabbe
Om Maurya <ommaurya2580@gmail.com>
2026-06-28 21:48:54 +0530

    Delete ToTestInt.java

--- Week4/Day6/OOPS/ToTestInt.java
+++
@@ -1,9 +0,0 @@
-public class ToTestInt {
-
-    public static void main(String[] args) {
-
-        Arithmetic obj = new Arithmetic();
-
-        System.out.println("Square of 5 = " + obj.square(5));
-    }
-}
